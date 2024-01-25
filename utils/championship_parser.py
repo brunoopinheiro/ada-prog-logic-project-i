a7b3699b3610bd518674a7c0b7502077ddff4049
brunopinheiro <bpinheiro@hie.tec.br>
2024-01-25 13:07:49 -0300

    feat: organizacao projeto

---
+++ utils/championship_parser.py
@@ -0,0 +1,8 @@
+# Deve ser a funcao base que vai ler o input e retornar a tabela do campeonato
+def championship_parser(input_json: dict):
+    """
+    Função principal, que recebe o dict de input do campeonato e monta a tabela final com a classificação dos times, ordenada seguindo os critérios de classificação do campeonato.
+    """
+    output_table = []
+    rodadas = input_json.keys()
+    return rodadas
