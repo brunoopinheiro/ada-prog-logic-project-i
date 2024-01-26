b464441a5d513d3ad5bd81d16a863886401a9de7
Bruno Pinheiro <93209918+brunoopinheiro@users.noreply.github.com>
2024-01-25 22:06:04 -0300

    feat: le partidas (incorreto)

--- utils/championship_parser.py
+++ utils/championship_parser.py
@@ -1,8 +1,23 @@
+from utils import match_analysis as ma
+
+
 # Deve ser a funcao base que vai ler o input e retornar a tabela do campeonato
 def championship_parser(input_json: dict):
     """
-    Função principal, que recebe o dict de input do campeonato e monta a tabela final com a classificação dos times, ordenada seguindo os critérios de classificação do campeonato.
+    Função principal do projeto.
+    Recebe o dict de input do campeonato,
+    e monta a tabela final com a classificação dos times.
+    Output ordenado seguindo os critérios de classificação do campeonato.
     """
     output_table = []
     rodadas = input_json.keys()
-    return rodadas
+    for rodada in rodadas:
+        partidas = input_json[rodada]
+        n_partidas = partidas.keys()
+        for partida in n_partidas:
+            home, away = ma.match_analysis(n_partidas[partida])
+            # Temporario
+            output_table.append(home)
+            output_table.append(away)
+
+    return output_table
---
+++ utils/match_analysis.py
@@ -0,0 +1,10 @@
+def match_analysis(match: dict):
+    """
+    Recebe um dict da partida.
+    Retorna uma tupla com o resultado da partida.
+    """
+    home_club = {"name": match.clubs.home}
+    away_club = {"name": match.clubs.away}
+
+    return (home_club, away_club)
+    ...
