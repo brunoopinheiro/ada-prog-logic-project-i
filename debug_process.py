84102c0d0488c03301dd42e89f7db256efd110f8
Bruno Pinheiro <93209918+brunoopinheiro@users.noreply.github.com>
2024-01-27 13:35:25 -0300

    feat: adiciona grafico scatter ao projeto

--- debug_process.py
+++ debug_process.py
@@ -1,14 +1,11 @@
 import pandas as pd
 from utils import championship_parser as cp
-from utils import coach_handler as ch
+
+# from utils import coach_handler as ch
+# import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
     input_json = pd.read_json("./input_files/brasileirao-2019.json").to_dict()
-    tabela_br = cp.championship_parser(input_json)
-
-    coaches = [ch.lasting_coach(club) for club in tabela_br]
-    print(coaches)
-    # for team in tabela_br:
-    #     print(team)
-    #     print("----")
+    tabela_brasileirao = cp.championship_parser(input_json)
+    print(tabela_brasileirao)
